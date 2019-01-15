8bab70617a8fea646741d653fc5670705ae7f92e
Lim Kai Jie <lim.kai.jie@celcom.com.my>
2019-01-15 11:51:44 +0800

    fixed bug

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 var map = {}; // You could also use an array
 var isDone = false;
+var failedQuery = false;
 
 onkeydown = onkeyup = function(e) {
     e = e || event; // to deal with IE
     map[e.keyCode] = e.type == 'keyup';
 
-    if (map[17] && map[13]) { // CTRL + ENTER
-        run();
+    if (map[17] && map[13]){ // CTRL + ENTER
+        if (failedQuery == false) {run();}
+        else {console.log('[INFO] Previous query failed!');}
         map = {};
     }
 }
@@ -16,21 +18,24 @@ function run(){
     isDone = window.setInterval(checkIfTabPaneActive, 1000);
 }
 
-function checkIfTabPaneActive(){
-
-    try{
+function checkIfTabPaneActive() {
+    try {
         var result = document.getElementById('queryResults').className;
-        console.log('trying');
-        if (result == 'tab-pane active'){
+        console.log('[INFO] Listening...')
+        // If result is displayed
+        if (result == 'tab-pane active') {
+            // Send notification
             chrome.runtime.sendMessage('trigger');
-            window.clearTimeout(isDone);
-            console.log('done');
+            console.log('[INFO] Successfully notified!');
+            // Update query status
+            failedQuery = false;
+            // Clear notification
+            window.clearInterval(isDone);
+        }else {
+            failedQuery = true;
         }
     }
-    catch(TypeError){
-        window.clearTimeout(isDone);
-        console.log('catching');
+    catch(TypeError) {
+        window.clearInterval(isDone);
     }
-
-
 }
